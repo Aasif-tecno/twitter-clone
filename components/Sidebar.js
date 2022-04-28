5381e3e40ca1f2dfb6dcacff51af706b519a47f2
Mohammed Aasif <mohammedaasif@tecnovators.in>
2022-04-28 10:57:53 +0530

    #Inial Commit

---
+++ components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+
+function Sidebar() {
+  return (
+    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed">
+      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
+        <Image src="https://rb.gy/ogau5a" width={30} height={30} alt="Logo" />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
